5ed7a7891aefb662707e49a246f7de981b857199
Bojo <bozhidar_zahariev@hotmail.com>
2020-05-27 13:33:18 +0100

    trans

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const element = document.querySelector('#bin');
-var rect = element.getBoundingClientRect();
+let topM = '';
+let leftM = '';
+
+const lorry = document.querySelector('#lorry');
+
+const bin = document.querySelector('#bin');
+var rect = bin.getBoundingClientRect();
 console.log(rect.top, rect.left);
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', e => {
+  lorry.style.marginLeft = '300px';
+});
