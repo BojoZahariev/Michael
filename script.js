05c3afe92452a469bcfe663ac9c9f0cff7515639
Bojo <bozhidar_zahariev@hotmail.com>
2020-05-28 09:29:34 +0100

    bin3

--- script.js
+++ script.js
@@ -6,19 +6,23 @@ let leftM = rect.left;
 const lorry = document.querySelector('#lorry');
 
 const bin1 = document.querySelector('#bin1');
+const bin2 = document.querySelector('#bin2');
+const bin3 = document.querySelector('#bin3');
+
 bin1.addEventListener('click', e => {
   var rect = bin1.getBoundingClientRect();
-
   lorry.style.marginLeft = `${rect.left}px`;
   lorry.style.top = `${rect.top}px`;
-  console.log(rect.top, rect.left);
 });
 
-const bin2 = document.querySelector('#bin2');
 bin2.addEventListener('click', e => {
   var rect = bin2.getBoundingClientRect();
-
   lorry.style.marginLeft = `${rect.left}px`;
   lorry.style.top = `${rect.top}px`;
-  console.log(rect.top, rect.left);
+});
+
+bin3.addEventListener('click', e => {
+  var rect = bin3.getBoundingClientRect();
+  lorry.style.marginLeft = `${rect.left}px`;
+  lorry.style.top = `${rect.top}px`;
 });
