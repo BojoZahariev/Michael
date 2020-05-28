1a02d44d031b5d834d63dd1cdc15ecc6fc7cb8f6
Bojo <bozhidar_zahariev@hotmail.com>
2020-05-28 12:35:33 +0100

    new

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let topM = rect.top;
 let leftM = rect.left;
 
 */
+let moved = false;
 const lorry = document.querySelector('#lorry');
 
 const destination1 = document.querySelector('#destination1');
@@ -10,19 +11,28 @@ const destination2 = document.querySelector('#destination2');
 const destination3 = document.querySelector('#destination3');
 
 destination1.addEventListener('click', e => {
-  var rect = destination1.getBoundingClientRect();
-  lorry.style.left = `${rect.left}px`;
-  lorry.style.top = `${rect.top}px`;
+  if (!moved) {
+    var rect = destination1.getBoundingClientRect();
+    lorry.style.left = `${rect.left}px`;
+    lorry.style.top = `${rect.top}px`;
+    moved = true;
+  }
 });
 
 destination2.addEventListener('click', e => {
-  var rect = destination2.getBoundingClientRect();
-  lorry.style.marginLeft = `${rect.left}px`;
-  lorry.style.top = `${rect.top}px`;
+  if (!moved) {
+    var rect = destination2.getBoundingClientRect();
+    lorry.style.marginLeft = `${rect.left}px`;
+    lorry.style.top = `${rect.top}px`;
+    moved = true;
+  }
 });
 
 destination3.addEventListener('click', e => {
-  var rect = destination3.getBoundingClientRect();
-  lorry.style.marginLeft = `${rect.left}px`;
-  lorry.style.top = `${rect.top}px`;
+  if (!moved) {
+    var rect = destination3.getBoundingClientRect();
+    lorry.style.marginLeft = `${rect.left}px`;
+    lorry.style.top = `${rect.top}px`;
+    moved = true;
+  }
 });
