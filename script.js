c066ddf7afdef5ec3ab9f2cd67f92757370e6781
Bojo <bozhidar_zahariev@hotmail.com>
2020-06-12 10:54:28 +0100

    timeout

--- script.js
+++ script.js
@@ -13,23 +13,54 @@ let match = destination2;
 const hero = document.querySelector('#hero');
 
 const checkMatch = vehicle => {
+  let rect = vehicle.getBoundingClientRect();
+  hero.style.left = `${rect.left}px`;
+  hero.style.top = `${rect.top + 15}px`;
   if (vehicle === match) {
-    var rect = vehicle.getBoundingClientRect();
-    hero.style.marginLeft = `${rect.left}px`;
-    hero.style.top = `${rect.top + 15}px`;
     moved = true;
     hero.classList.add('animatedMatch');
     vehicle.classList.add('animatedTruckMatch');
+  } else {
+    moved = true;
+    hero.classList.add('animatedNoMatch');
+
+    setTimeout(function() {
+      moved = false;
+      hero.classList.remove('animatedNoMatch');
+      hero.style.left = '100px';
+      hero.style.top = '50%';
+    }, 10000);
+  }
+};
+
+const heroPosition = () => {
+  let rect = hero.getBoundingClientRect();
+  console.log(rect.left);
+  if (rect.left === 100) {
+    hero.classList.remove('animatedNoMatch');
+    console.log('ding');
+    return true;
+  } else {
+    return false;
   }
 };
 
 destination1.addEventListener('click', e => {
   if (!moved) {
+    checkMatch(destination1);
+    /*
     var rect = destination1.getBoundingClientRect();
     hero.style.left = `${rect.left}px`;
     hero.style.top = `${rect.top}px`;
     hero.classList.add('animatedNoMatch');
     moved = true;
+    setTimeout(function() {
+      moved = false;
+      hero.classList.remove('animatedNoMatch');
+      hero.style.left = '100px';
+      hero.style.top = '50%';
+    }, 10000);
+    */
   }
 });
 
@@ -41,10 +72,6 @@ destination2.addEventListener('click', e => {
 
 destination3.addEventListener('click', e => {
   if (!moved) {
-    var rect = destination3.getBoundingClientRect();
-    hero.style.left = `${rect.left}px`;
-    hero.style.top = `${rect.top}px`;
-    hero.classList.add('animatedNoMatch');
-    moved = true;
+    checkMatch(destination3);
   }
 });
